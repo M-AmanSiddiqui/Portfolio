7bc7c305d8c349b622ec227d364c2649c6fa1dc7
M-AmanSiddiqui <m.amansiddiqui2024@gmail.com>
2025-12-23 23:18:39 +0500

    edited

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -99,7 +99,7 @@ const Contact = () => {
           </label>
 
           <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Email</span>
+            <span className="text-white font-medium mb-4">Your Emai</span>
             <input
               type="email"
               name="email"
